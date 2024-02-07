33ecdc0ffa3305eff1a8e82cee089fb046a1ed20
Tapeky <tareq.sadouk@gmail.com>
2024-02-07 15:55:03 +0100

    ne pas oublier le philo%2 time_to_sleep/2 + 10

--- monitoring.c
+++ monitoring.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_data.c                                        :+:      :+:    :+:   */
+/*   monitoring.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 14:58:00 by tsadouk           #+#    #+#             */
-/*   Updated: 2024/01/31 15:00:22 by tsadouk          ###   ########.fr       */
+/*   Updated: 2024/02/07 15:43:54 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,20 @@ int	is_philo_dead(t_philo *philo)
 		change_philo_state(philo, DEAD);
 		print_philo_action(philo, "died");
 		return (1);
+	}
+	return (0);
+}
+
+int monitor_all_alive(t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos->data->nb_philos)
+	{
+		if (get_philo_state(&philos[i]) == DEAD)
+			return (1);
+		i++;
 	}
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 14:30:53 by tsadouk           #+#    #+#             */
-/*   Updated: 2024/02/07 15:15:36 by tsadouk          ###   ########.fr       */
+/*   Updated: 2024/02/07 15:44:06 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,6 @@ int	errors_handeler(int argc, char **argv);
 void print_args(char **argv);
 u_int64_t	get_time_in_ms(void);
 int	ft_atoi(const char *str);
-void	*monitor_all_alive(void *arg);
 t_state	get_philo_state(t_philo *philosopher);
 int	think(t_philo *philosopher);
 int	is_input_valid(char **argv);
@@ -113,6 +112,13 @@ uint64_t	get_start_time(t_data *data);
 uint64_t	get_time(void);
 void	ft_usleep(uint64_t time);
 void	wait_until(u_int64_t wakeup_time);
+
+// MONITORING
+
+int monitor_all_alive(t_philo *philos);
+int	is_philo_dead(t_philo *philo);
+int	is_philo_full(t_philo *philo);
+
 
 
 
